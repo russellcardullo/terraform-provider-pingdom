7abb09ef80595f68a840166dc336483aebdbdd66
Jay Ding <jding@scentregroup.com>
2018-10-29 11:03:50 +1100

    Change upstream repo back for pull request

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/jayhding/terraform-provider-pingdom/pingdom"
+	"github.com/russellcardullo/terraform-provider-pingdom/pingdom"
 )
 
 func main() {
